5e6e028996762035d15930ff21e960dcf2a4160d
aidanahn <aidanahn08@gmail.com>
2025-08-10 16:25:22 -0400

    Reworked ESC test to run for 5 seconds

--- tests/motor_test/src/main.cpp
+++ tests/motor_test/src/main.cpp
@@ -6,14 +6,28 @@ const int FREQUENCY = 50;
 const int RESOLUTION = 16;
 const int LED_CHANNEL = 0;
 
+void setESCMicroseconds(int microseconds) {
+  ledcWrite(LED_CHANNEL, int((double(microseconds) / 20000) * 65535));
+}
+
 void setup() {
   Serial.begin(115200);
   ledcSetup(LED_CHANNEL, FREQUENCY, RESOLUTION);
   ledcAttachPin(ESC_PIN, LED_CHANNEL);
+  setESCMicroseconds(1500);
+  Serial.println("Turn on ESC");
+  delay(5000);
+  Serial.println("Starting Test");
 }
 
 void loop() {
-  ledcWrite(LED_CHANNEL, 4915);
-  Serial.println("Neutral");
-  delay(1000);
+  setESCMicroseconds(1450);
+  delay(5000);
+  while (true) {
+    setESCMicroseconds(1500);
+    Serial.println("Test Finished");
+  }
+  
 }
+
+
